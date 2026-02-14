e18caf2851bb6d44edd994af1d8cd2cbc7838793
Admin Adin Greenwald <ga.adin.greenwald@kodiaksolutions.io>
2026-02-13 21:07:35 -0500

    ACR tags

--- terraform/acr/main.tf
+++ terraform/acr/main.tf
@@ -2,11 +2,21 @@ data "azurerm_resource_group" "rg" {
   name     = "aks-hello-world-poc"
 }
 
+locals {
+  tags = {
+    "Owner 1"         : "agreenwald@westmonroe.com"
+    "Owner 2"         : "None"
+    "Client Code"     : "Jepp-POC"
+  }
+}
+
 resource "azurerm_container_registry" "hello_world" {
   name                = "wmpagreenwaldhelloworldacr"
   resource_group_name = data.azurerm_resource_group.rg.name
   location            = data.azurerm_resource_group.rg.location
   sku                 = "Basic"
+
+  tags = local.tags
 
   lifecycle {
     prevent_destroy = true
